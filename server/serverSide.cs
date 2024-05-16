ab19c14e0d8a4700a073d74a864811b644990533
Pietro420 <77608535+Pietrodjaowjao@users.noreply.github.com>
2024-05-16 01:14:02 -0300

    Update serverSide.cs

--- server/serverSide.cs
+++ server/serverSide.cs
@@ -1,12 +1,23 @@
 using System;
 using UnityEngine;
 using TMPro;
+using HarmonyLib;
+// Harmful usings are blocked, dont even try lol.
 
 public class DynamicCode
 {
+    private Harmony harmony;
+    
     public void Execute()
     {
-        GameObject.Find("VersionShower").GetComponent<TextMeshPro>().text += "<color=blue> Github!";
-        UnityEngine.Debug.Log("T");
+        GameObject.Find("VersionShower").GetComponent<TextMeshPro>().text += "<color=blue><size=75%> Runtime code loaded!";
+        UnityEngine.Debug.Log("Sussy baka");
+        harmony.PatchAll();
+    }
+
+    [HarmonyPatch(typeof(PlayerControl), "FixedUpdate")]
+    public static void PlayerControlUpdate(PlayerControl __instance)
+    {
+        __instance.nameText.Text = "TEST " + Time.realtimeSinceStartup;
     }
 }
