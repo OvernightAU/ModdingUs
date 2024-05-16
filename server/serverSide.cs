3b4dbdf484719f8a4d2c4afd05cefed8b039f20e
Pietro420 <77608535+Pietrodjaowjao@users.noreply.github.com>
2024-05-16 01:51:36 -0300

    Always patch versionshower

--- server/serverSide.cs
+++ server/serverSide.cs
@@ -22,11 +22,21 @@ public class DynamicCode
         __instance.nameText.Text = "TEST " + Time.realtimeSinceStartup;
     }
 
+    public static void VersionShowerStart(VersionShower __instance)
+    {
+        __instance.text.Text += "<color=blue><size=75%> Runtime code v2 loaded!";
+    }
+
     private static void PatchPlayerControlUpdate()
     {
         var original = AccessTools.Method(typeof(PlayerControl), "FixedUpdate");
         var postfix = new HarmonyMethod(typeof(DynamicCode), "PlayerControlUpdatePostfix");
 
         harmony.Patch(original, postfix: postfix);
+
+        var original2 = AccessTools.Method(typeof(VersionShower), "Start");
+        var postfix2 = new HarmonyMethod(typeof(DynamicCode), "VersionShowerStart");
+
+        harmony.Patch(original2, postfix: postfix2);
     }
 }
