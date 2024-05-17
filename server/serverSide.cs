da9bae16373c27200059457181202ce0f0c593be
Pietro420 <77608535+Pietrodjaowjao@users.noreply.github.com>
2024-05-16 23:35:24 -0300

    Update serverSide.cs

--- server/serverSide.cs
+++ server/serverSide.cs
@@ -7,37 +7,13 @@ using HarmonyLib;
 
 public class DynamicCode
 {
-    private static Harmony harmony;
+    private static bool already = false;
     
     public void Execute()
     {
-        if (harmony != null) return;
-        harmony = new Harmony("com.example.dynamiccode");
-        GameObject.Find("VersionShower").GetComponent<TextMeshPro>().text += "<color=blue><size=75%> Runtime code v2 loaded!";
+        if (already) return;
+        already = true;
+        GameObject.Find("VersionShower").GetComponent<TextMeshPro>().text += "<color=blue><size=75%> GITHUB LOADED!";
         UnityEngine.Debug.Log("Sussy baka");
-        PatchPlayerControlUpdate();
-    }
-
-    public static void PlayerControlUpdatePostfix(PlayerControl __instance)
-    {
-        __instance.nameText.Text = "TEST " + Time.realtimeSinceStartup;
-    }
-
-    public static void VersionShowerStart(VersionShower __instance)
-    {
-        __instance.text.text += "<color=blue><size=75%> Runtime code v2 loaded!";
-    }
-
-    private static void PatchPlayerControlUpdate()
-    {
-        var original = AccessTools.Method(typeof(PlayerControl), "FixedUpdate");
-        var postfix = new HarmonyMethod(typeof(DynamicCode), "PlayerControlUpdatePostfix");
-
-        harmony.Patch(original, postfix: postfix);
-
-        var original2 = AccessTools.Method(typeof(VersionShower), "Start");
-        var postfix2 = new HarmonyMethod(typeof(DynamicCode), "VersionShowerStart");
-
-        harmony.Patch(original2, postfix: postfix2);
     }
 }
