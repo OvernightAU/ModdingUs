57e86fc77b5c44cd72f784f595e7c4427743e427
Pietro420 <77608535+Pietrodjaowjao@users.noreply.github.com>
2024-05-24 21:13:16 -0300

    Update serverSide.cs

--- server/serverSide.cs
+++ server/serverSide.cs
@@ -6,14 +6,9 @@ using HarmonyLib;
 // Harmony is not recommended for several compatibility reasons, so you should make event-based things instead of using harmony
 
 public class DynamicCode
-{
-    private static bool already = false;
-    
+{    
     public void Execute()
     {
-        if (already) return;
-        already = true;
-        GameObject.Find("VersionShower").GetComponent<TextMeshPro>().text += "<color=blue><size=75%> GITHUB LOADED!";
         UnityEngine.Debug.Log("Sussy baka");
     }
 }
