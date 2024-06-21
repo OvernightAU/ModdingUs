3862f7e2602ac1d48ad7cb737b27efba3e37533f
Pietro420 <77608535+Pietrodjaowjao@users.noreply.github.com>
2024-06-21 01:22:07 -0300

    Update serverSide.cs

--- server/serverSide.cs
+++ server/serverSide.cs
@@ -2,7 +2,7 @@ using System;
 using UnityEngine;
 using TMPro;
 using HarmonyLib;
-using Kino;
+using UnityEngine.SceneManagement;
 // Harmful usings are blocked, dont even try lol.
 // Harmony is not recommended for several compatibility reasons, so you should make event-based things instead of using harmony
 
