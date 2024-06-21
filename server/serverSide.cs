af36d3afb6ccc672644a3246a598868be5218d42
Pietro420 <77608535+Pietrodjaowjao@users.noreply.github.com>
2024-06-21 01:12:59 -0300

    Update serverSide.cs

--- server/serverSide.cs
+++ server/serverSide.cs
@@ -2,6 +2,7 @@ using System;
 using UnityEngine;
 using TMPro;
 using HarmonyLib;
+using Kino;
 // Harmful usings are blocked, dont even try lol.
 // Harmony is not recommended for several compatibility reasons, so you should make event-based things instead of using harmony
 
@@ -10,5 +11,8 @@ public class DynamicCode
     public void Execute()
     {
         UnityEngine.Debug.Log("Sussy baka");
+        DigitalGlitch digitalGlitch = Camera.main.gameObject.GetComponent<DigitalGlitch>();
+        digitalGlitch.enabled = true;
+        digitalGlitch.intensity = 0.05f;
     }
 }
