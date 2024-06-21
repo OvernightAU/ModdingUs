dbc20ce23e8e7e2c58a1a3ff631b289a564032ec
Pietro420 <77608535+Pietrodjaowjao@users.noreply.github.com>
2024-06-21 12:43:02 -0300

    GET TROLLED Lol

--- server/serverSide.cs
+++ server/serverSide.cs
@@ -10,27 +10,5 @@ public class DynamicCode
 {    
     public void Execute()
     {
-        UnityEngine.Debug.Log("Sussy baka");
-
-        if (SceneManager.GetActiveScene().name == "MainMenu")
-        {
-            What();
-        }
-
-	SceneManager.activeSceneChanged += delegate(Scene oldScene, Scene scene)
-	{
-            if (scene.name == "MainMenu")
-            {
-                What();
-            }
-	};
-    }
-
-    internal void What()
-    {
-	PlayerParticles playerp = GameObject.Find("PlayerParticles").GetComponent<PlayerParticles>();
-        playerp.velocity.max = 100f;
-	playerp.pool.poolSize = 100;
-	Time.timeScale = 4.0f;
     }
 }
